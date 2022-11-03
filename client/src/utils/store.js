9b8d4b7cf01a3f66b70898172de095761dc69eef
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 17:55:21 +0000

    amended back to createStore for now

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,6 +1,6 @@
-// import { createStore } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+import { legacy_createStore as createStore } from "redux";
 import reducers from "./reducers";
 import { composeWithDevTools } from "@redux-devtools/extension";
 
-export default store = configureStore(reducers);
+export default createStore(reducers);
