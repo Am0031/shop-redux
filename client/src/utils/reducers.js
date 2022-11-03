97b286aede614a90abb451ea2426297b7b3a29b9
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 16:57:32 +0000

    added base in utils/reducers

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,8 @@
+import { useReducer } from "react";
+
+const reducers = () => {
+  //add all reducer cases with switch/case syntax
+  //depends on initial state/state and actions
+};
+
+export default reducers;
