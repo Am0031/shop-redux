b07f91b373e1e33dcb0096c8e7a9dd4bede05c8c
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 16:38:25 +0000

    added httpLink and authLink

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,21 @@ import {
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL || "/graphql",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
