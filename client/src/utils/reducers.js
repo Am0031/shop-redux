09d6bca1e373f5abde82574c8c5b49a4a9d9835e
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 17:02:18 +0000

    import actions in reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,14 @@
-import { useReducer } from "react";
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from "./actions";
 
 const reducers = () => {
   //add all reducer cases with switch/case syntax
