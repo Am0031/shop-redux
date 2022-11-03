87d48963ba023d533aa706ab4d3055a68a42fa49
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 16:52:50 +0000

    added Nav to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,11 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
+import { Provider } from "react-redux";
+
 import Home from "./containers/Home";
+import Nav from "./components/Nav";
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL || "/graphql",
@@ -34,6 +38,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="App">
+          <Nav />
           <Switch>
             <Route exact path="/" element={Home} />
           </Switch>
