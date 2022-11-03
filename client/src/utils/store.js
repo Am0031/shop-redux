6fec18efcac74190d29d61bf0f2653d5aecd961f
Amelie Pira <amelie.pira@gmail.com>
2022-11-03 17:49:30 +0000

    added reduxjs/toolkit and amended store.js

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,4 +1,6 @@
-import { createStore } from "redux";
+// import { createStore } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducers from "./reducers";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-export default createStore(reducers);
+export default store = configureStore(reducers);
