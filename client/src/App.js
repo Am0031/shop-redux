857cec9a0cc5986e53aa0a97ee66d9bb751c6ec0
Amelie Pira <amelie.pira@gmail.com>
2022-11-04 16:24:13 +0000

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,12 @@ import { setContext } from "@apollo/client/link/context";
 import { Provider } from "react-redux";
 
 import Home from "./containers/Home";
+import Detail from "./containers/Detail";
+import NoMatch from "./containers/NoMatch";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import Success from "./containers/Success";
+import OrderHistory from "./containers/OrderHistory";
 import Nav from "./components/Nav";
 import store from "./utils/store";
 
@@ -43,6 +49,12 @@ function App() {
             <Nav />
             <Switch>
               <Route exact path="/" element={Home} />
+              <Route exact path="/login" element={Login} />
+              <Route exact path="/signup" element={Signup} />
+              <Route exact path="/success" element={Success} />
+              <Route exact path="/orderHistory" element={OrderHistory} />
+              <Route exact path="/products/:id" element={Detail} />
+              <Route component={NoMatch} />
             </Switch>
           </Provider>
         </div>
