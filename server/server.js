cf23c5114a28491325dd41d3af2e59288fa98a57
Amelie Pira <amelie.pira@gmail.com>
2022-11-04 13:29:43 +0000

    set up server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,44 @@
-console.log("server.js");
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//creating apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+//express is called as middleware of apollo server
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"));
+});
+
+//making express middleware for apollo server
+const startServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
+};
+
+startServer(typeDefs, resolvers);
