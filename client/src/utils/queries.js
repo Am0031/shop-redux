b974eedeec5ec331260c0dfb7118af3fa8029f88
Amelie Pira <amelie.pira@gmail.com>
2022-11-04 16:15:00 +0000

    added graphql queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,70 @@
-//will contain all queries to the server
+import { gql } from "@apollo/client";
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    getProducts(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    getProducts {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    getCategories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    getUser {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
