adeee9c0ca2c2aed6d8f9a5305ef9373205f762f
Amelie Pira <amelie.pira@gmail.com>
2022-11-04 18:04:40 +0000

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
 import { Provider } from "react-redux";
 
@@ -46,15 +46,15 @@ function App() {
         <div className="App">
           <Provider store={store}>
             <Nav />
-            <Switch>
-              <Route exact path="/" element={Home} />
-              <Route exact path="/login" element={Login} />
-              <Route exact path="/signup" element={Signup} />
-              <Route exact path="/success" element={Success} />
-              <Route exact path="/orderHistory" element={OrderHistory} />
-              <Route exact path="/products/:id" element={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/success" element={<Success />} />
+              <Route exact path="/orderHistory" element={<OrderHistory />} />
+              <Route exact path="/products/:id" element={<Detail />} />
+              <Route component={<NoMatch />} />
+            </Routes>
           </Provider>
         </div>
       </Router>
