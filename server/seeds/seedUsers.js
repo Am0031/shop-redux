8b30233265e0d5564e3fb6e7736db82073258e90
Amelie Pira <amelie.pira@gmail.com>
2022-11-04 14:24:39 +0000

    added seedUsers

---
+++ server/seeds/seedUsers.js
@@ -0,0 +1,26 @@
+const { User } = require("../models");
+
+const seedUsers = async () => {
+  await User.create({
+    firstName: "Pamela",
+    lastName: "Washington",
+    email: "pamela@testmail.com",
+    password: "password123",
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id],
+      },
+    ],
+  });
+
+  await User.create({
+    firstName: "Elijah",
+    lastName: "Holt",
+    email: "eholt@testmail.com",
+    password: "password123",
+  });
+
+  console.log("users successfully seeded");
+};
+
+module.exports = { seedUsers };
